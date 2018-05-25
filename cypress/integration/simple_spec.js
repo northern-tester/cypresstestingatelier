241ddd4d06eadb9e139fbdfda4f48792d347a511
Ash Winter <ash_winter@hotmail.co.uk>
2018-05-25 16:45:44 +0100

    A few simple specs

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,8 +1,32 @@
-describe('My First Test', function() {
-    it('finds the content Testing Atelier Site', function() {
-      cy.visit('http://testingatelier.community')
-      cy.contains('Sponsor Us').click()
-      
-      cy.url().should('include', '/sponsors')
+describe('My First Test', function(){
+    it('Does not do much!', function(){
+        expect(true).to.equal(true)
     })
-  })
+})
+
+describe('My Second Test', function(){
+    it('Does not do much!', function(){
+        expect(false).to.equal(false)
+    })
+})
+
+describe('My Third Test', function(){
+    it('Finds the content "type" in the example app', function(){
+        cy.visit('https://example.cypress.io')
+        cy.contains('type').click()
+        //Should be on a new url which includes '/commands/actions'
+        cy.url().should('include', '/commands/actions')
+    })
+})
+
+describe('My Fourth Test', function(){
+    it('Types an email and asserts against it', function(){
+        cy.visit('https://example.cypress.io')
+        cy.contains('type').click()
+        //Should be on a new url which includes '/commands/actions'
+        cy.url().should('include', '/commands/actions')
+        cy.get('.action-email')
+          .type('fake@email.com')
+          .should('have.value', 'fake@email.com')
+    })
+})
